ce5f8f69b6cd4ecb7519c4fc06cd262322c1b118
Nander <nander@nander.dev>
2021-12-14 21:46:16 +0000

    update example

--- src/routes/api/test.ts
+++ src/routes/api/test.ts
@@ -1,14 +1,16 @@
+import { env } from 'process';
+
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get() {
 	const first = import.meta.env.SUPERSECRET_VARIABLE;
 	const second = import.meta.env['SUPERSECRET_VARIABLE'];
 	const third = process.env.SUPERSECRET_VARIABLE;
 	const fourth = process.env['SUPERSECRET_VARIABLE'];
+	const fifth = env.SUPERSECRET_VARIABLE;
 
-	// undefined undefined undefined undefined
-	console.log(first, second, third, fourth);
+	console.log(first, second, third, fourth, fifth);
 
 	return {
-		body: { response: first || second || third || fourth }
+		body: { response: first || second || third || fourth || fifth }
 	};
 }
